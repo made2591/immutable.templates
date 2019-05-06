59de3d2fa9e657be6c362151d30a2859bcdeb33a
made2591 <matteo.madeddu@gmail.com>
2019-05-06 17:52:02 +0200

    Add sns lambda

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# immutable.run
+# immutable.templates
+
+Immutable templates contains a collection of CDK templates. The respective CloudFormation templates are store in [https://github.com/GuidoNebiolo/aws-architectures](https://github.com/GuidoNebiolo/aws-architectures)
+
+## Arhictectures
---
+++ templates/contact-form/lib/sns-lambda/index.js
@@ -0,0 +1,31 @@
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+
+const snstopic = process.env.TOPIC_ARN
+const region = process.env.REGION || 'eu-west-1'
+AWS.config.update({region: region});
+
+exports.handler = function(event, context, callback) {
+    console.log(JSON.stringify(event, null, 2));
+    event.Records.forEach(function(record) {
+        console.log(record.eventID);
+        console.log(record.eventName);
+        console.log('DynamoDB Record: %j', record.dynamodb);
+        var publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(
+            {
+                Message: JSON.stringify(record.dynamodb),
+                TopicArn: snstopic
+            }
+        ).promise();
+        publishTextPromise.then(
+            function(data) {
+                console.log("Message send sent to the topic {snstopic}");
+                console.log("MessageID is " + data.MessageId);
+            }).catch(
+                function(err) {
+                console.error(err, err.stack);
+            }
+        );
+    });
+    callback(null, "message"); 
+};
