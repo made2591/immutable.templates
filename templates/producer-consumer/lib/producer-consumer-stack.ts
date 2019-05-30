df1db7c1f6227cf31cdd828721547121c22004b3
Matteo Madeddu <matteo.madeddu@gmail.com>
2019-05-30 11:09:02 +0200

    Introducing SQS S3 bucket notification

--- templates/producer-consumer/lib/producer-consumer-stack.ts
+++ templates/producer-consumer/lib/producer-consumer-stack.ts
@@ -1,9 +1,13 @@
 import cdk = require('@aws-cdk/cdk');
+import cloudtrail = require('@aws-cdk/aws-cloudtrail');
+import iam = require('@aws-cdk/aws-iam');
 import sqs = require('@aws-cdk/aws-sqs');
 
 import { UploadFormStack, UploadFormStackProps } from "../../upload-form/lib/upload-form-stack";
+import { PolicyStatementEffect } from '@aws-cdk/aws-iam';
+import { ReadWriteType } from '@aws-cdk/aws-cloudtrail';
 
-interface ProducerConsumerStackProps extends UploadFormStackProps {
+export interface ProducerConsumerStackProps extends UploadFormStackProps {
 }
 
 export interface ProducerConsumerStack extends UploadFormStack {
@@ -14,10 +18,33 @@ export class ProducerConsumerStack extends UploadFormStack {
   constructor(scope: cdk.Construct, id: string, props: ProducerConsumerStackProps) {
     super(scope, id, props);
 
-    this.sqsQueue = new sqs.Queue(this, 'Queue', {
+    // create trail to enable events on s3
+    const trail = new cloudtrail.Trail(this, props.stage.toString() + 'content-bucket-trail');
+    trail.addS3EventSelector([this.contentBucket.bucketArn + "/"], {
+      includeManagementEvents: false,
+      readWriteType: ReadWriteType.All,
+    });
+
+    // create sqs queue
+    this.sqsQueue = new sqs.Queue(this, props.stage.toString() + "-sqs-queue", {
       retentionPeriodSec: 1209600
     });
-    this.contentBucket.onPutObject(props.stage.toString() + "-put-event");
+
+    // create sqs policy statement to allow cloudwatch pushing the message
+    var s3SQSStatement = new iam.PolicyStatement(PolicyStatementEffect.Allow)
+    s3SQSStatement.addActions(
+      "sqs:SendMessage",
+      "sqs:SendMessageBatch",
+      "sqs:GetQueueAttributes",
+      "sqs:GetQueueUrl",
+    )
+    s3SQSStatement.addResources(
+      this.sqsQueue.queueArn
+    );
+    s3SQSStatement.addAnyPrincipal()
+    s3SQSStatement.addCondition("ArnLike", { "aws:SourceArn": this.contentBucket.bucketArn })
+
+    this.contentBucket.addObjectCreatedNotification(this.sqsQueue)
 
   }
 }
